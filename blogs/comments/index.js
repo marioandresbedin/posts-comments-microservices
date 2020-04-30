697fff2329a1d070691f8bb1164490db6eb9b76d
marioandresbedin <mario.andres.bedin@gmail.com>
2020-04-30 12:41:59 -0300

    IDP-1: Updating client app files  to generate Full React App.

--- blogs/comments/index.js
+++ blogs/comments/index.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-const comments = {};
+app.use(cors());
+
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(comments);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-  const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-  posts[id] = {
-    id, title
-  };
+  const comments = commentsByPostId[req.params.id] || [];
 
-  res.status(201).send(posts[id]);
+  comments.push({ id: commentId, content });
 
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-    console.log('Listening on 4000');
+  console.log('Listening on 4001');
 });
--- blogs/posts/index.js
+++ blogs/posts/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 const posts = {};
 
 app.get('/posts', (req, res) => {
@@ -15,13 +18,13 @@ app.post('/posts', (req, res) => {
   const { title } = req.body;
 
   posts[id] = {
-    id, title
+    id,
+    title
   };
 
   res.status(201).send(posts[id]);
-
 });
 
 app.listen(4000, () => {
-    console.log('Listening on 4000');
+  console.log('Listening on 4000');
 });
